f177389e2a711566306b7179c8ba4c5d6f4236e2
Filip Buljan <filipbuljan2002@gmail.com>
2024-05-17 08:35:56 +0200

    Implement generic repository

---
+++ Repository/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using PennyPlanner.Models;
+using System.Linq.Expressions;
+
+namespace PennyPlanner.Repository
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<List<T>> GetFilteredAsync(Expression<Func<T, bool>>[] filters, int? skip, int? take, params Expression<Func<T, object>>[] includes);
+        Task<List<T>> GetAsync(int? skip, int? take, params Expression<Func<T, object>>[] includes);
+        Task<T?> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes);
+        Task<int> InsertAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task SaveChangesAsync();
+    }
+}
