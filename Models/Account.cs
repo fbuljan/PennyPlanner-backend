3aab284473b13e3cf3f6ef1868a3ea5c7142c3ad
Filip Buljan <filipbuljan2002@gmail.com>
2024-05-18 20:48:18 +0200

    Set up database migrations, add missing field to account entity

--- Models/Account.cs
+++ Models/Account.cs
@@ -4,6 +4,7 @@
     {
         public User User { get; set; } = default!;
         public string Name { get; set; } = default!;
+        public float Balance { get; set; }
         public string? Description { get; set; }
     }
 }
